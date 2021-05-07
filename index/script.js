14a23f6fcbc159f4118efb153bebc6c19883f43c
Payd3r <payd3r2003@gmail.com>
2021-05-07 20:54:57 +0200

    e

---
+++ index/script.js
@@ -0,0 +1,97 @@
+var app = new Vue({
+    el: "#contenitore-vue",
+    data: {
+        voci_menu: [
+            {
+                nome: "Instagram",
+                img: " ",
+                link: "https://www.instagram.com/_.andre2903._/?hl=it"
+            }, {
+                nome: "Twitch",
+                img: " ",
+                link: "https://www.twitch.tv/payd3r"
+            }
+        ],
+        voci_teoria: [
+            {
+                data: "29 marzo 2021",
+                titolo: "es1 Khan Accademy",
+                link: "https://payd3r.github.io/KhanAccademy/Docs/es1.html"
+            },
+            {
+                data: "29 marzo 2021",
+                titolo: "es2 Khan Accademy",
+                link: "https://payd3r.github.io/KhanAccademy/Docs/es2.html"
+            },
+            {
+                data: "29 marzo 2021",
+                titolo: "es3 Khan Accademy",
+                link: "https://payd3r.github.io/KhanAccademy/Docs/es3.html"
+            },
+            {
+                data: "9 aprile 2021",
+                titolo: "Viaggi Khan Accademy",
+                link: "https://payd3r.github.io/KhanAccademy/Docs/viaggi.html"
+            },
+            {
+                data: "9 aprile 2021",
+                titolo: "Blog Khan Accademy",
+                link: "https://payd3r.github.io/KhanAccademy/Docs/blog.html"
+            },
+            {
+                data: "21 aprile 2021",
+                titolo: "Tag select Dinamico",
+                link: "https://payd3r.github.io/Telefonia/"
+            },
+            {
+                data: "23 aprile 2021",
+                titolo: "PCTO",
+                link: "https://payd3r.github.io/PCTO/"
+            }
+        ],
+        voci_lab: [
+            {
+                data: "Data",
+                titolo: "Titolo",
+                link: ""
+            },
+            {
+                data: "2 aprile 2021",
+                titolo: "Sito Influencer",
+                link: "https://payd3r.github.io/Influencer/Docs/Index.html"
+            },
+            {
+                data: "16 aprile 2021",
+                titolo: "Calcolo del peso di un oggetto in metallo",
+                link: "https://payd3r.github.io/CalcolaPesoOgg/CalcolaPesoOgg.html"
+            },
+            {
+                data: "15 aprile 2021",
+                titolo: "Biglietto treno",
+                link: "https://payd3r.github.io/Biglietto/"
+            },
+            {
+                data: "22 aprile 2021",
+                titolo: "Calcolo Peso Forma",
+                link: "https://payd3r.github.io/Formule/"
+            },
+            {
+                data: "3 Maggio 2021",
+                titolo: "Logo olimpiadi",
+                link: "https://payd3r.github.io/Olimpiadi/"
+            },
+            {
+                data: "10 Maggio 2021",
+                titolo: "Primo form con generazione Pdf",
+                link: "https://payd3r.github.io/PDF1/"
+            },
+            {
+                data: "13 Maggio 2021",
+                titolo: "Assi cartesiani",
+                link: "https://payd3r.github.io/PianoCartesiano/"
+            }
+        ],
+        dark_mode: false
+
+    }
+})
